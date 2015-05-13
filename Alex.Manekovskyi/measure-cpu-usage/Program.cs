0d96272290f0faf16632efc90226c56873b31856
manekovskiy <manekovskiy@gmail.com>
2015-05-13 16:18:53 +0300

    Added two sample projects
    - How to measure CPU utilization
    - How to use output stream redirection

---
+++ Alex.Manekovskyi/measure-cpu-usage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace measure_cpu_usage
+{
+	/// <summary>
+	/// This sample shows how to measure the CPU utilization
+	/// by each process currently running.
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var processes = Process.GetProcesses();
+			var performanceCounters = new List<PerformanceCounter>();
+			foreach (var process in processes)
+			{
+				performanceCounters.Add(new PerformanceCounter("Process", "% Processor Time", process.ProcessName));
+			}
+
+			while (true)
+			{
+				Thread.Sleep(1000); // 1000ms = 1s
+				Console.SetCursorPosition(0, 0);
+				for (int processIndex = 0; processIndex < processes.Length; processIndex++)
+				{
+					Console.WriteLine("{0,-60}{1,-5}", processes[processIndex].ProcessName, performanceCounters[processIndex].NextValue().ToString("F2"));
+				}
+			}
+		}
+	}
+}
---
+++ Alex.Manekovskyi/redirect-process-standard-streams/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redirect_process_standard_streams
+{
+	/// <summary>
+	/// This sample shows how parent process could
+	/// intercept write calls to the child's process
+	/// standard output and error streams.
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WriteLine("Welcome to the colored WHERE command!", ConsoleColor.White);
+			WriteLine("Enter the command arguments or press the Enter key to display help reference: ", ConsoleColor.White);
+			var arguments = Console.ReadLine();
+			arguments = string.IsNullOrWhiteSpace(arguments) ? "/?" : arguments;
+
+			var whereProcess = new Process();
+			whereProcess.StartInfo = new ProcessStartInfo
+			{
+				FileName = "where",
+				UseShellExecute = false, // UseShellExecute should always set to false if streams redirection is used
+				Arguments = arguments,
+				RedirectStandardError = true,
+				RedirectStandardOutput = true
+			};
+			whereProcess.ErrorDataReceived += ErrorDataReceivedHandler;
+			whereProcess.OutputDataReceived += OutputDataReceivedHandler;
+
+			whereProcess.Start();
+
+			whereProcess.BeginOutputReadLine();
+			whereProcess.BeginErrorReadLine();
+
+			whereProcess.WaitForExit();
+		}
+
+		private static void OutputDataReceivedHandler(object sender, DataReceivedEventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(e.Data)) return;
+
+			WriteLine(e.Data, ConsoleColor.Green);
+		}
+
+		private static void ErrorDataReceivedHandler(object sender, DataReceivedEventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(e.Data)) return;
+
+			WriteLine(e.Data, ConsoleColor.Red);
+		}
+
+		private static void WriteLine(string line, ConsoleColor color)
+		{
+			var savedForegroundColor = Console.ForegroundColor;
+			Console.ForegroundColor = color;
+
+			Console.WriteLine(line);
+
+			Console.ForegroundColor = savedForegroundColor;
+		}
+	}
+}
