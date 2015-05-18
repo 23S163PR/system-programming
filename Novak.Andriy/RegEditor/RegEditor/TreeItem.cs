023aa3af823fe0714df54c783f83e7fbf4fbdf36
Andriy Novak <kruzerandriy@gmail.com>
2015-05-19 00:29:00 +0300

    add model for register

---
+++ Novak.Andriy/RegEditor/RegEditor/TreeItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RegEditor
+{
+	class TreeItem
+	{
+		public string Title { get; set; }
+
+		public ObservableCollection<TreeItem> Items { get; set; }
+
+		public TreeItem()
+		{
+			Items = new ObservableCollection<TreeItem>();
+		}	
+	}
+}
