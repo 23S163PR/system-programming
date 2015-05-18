ab42f0543a540b6b0103df422b31e896060e0248
Andriy Novak <kruzerandriy@gmail.com>
2015-05-19 00:19:15 +0300

    Init Registry Editor progect

---
+++ Novak.Andriy/RegEditor/RegEditor/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace RegEditor
+{
+	public partial class MainWindow : Window
+	{
+		private readonly RegestryEditor _regestryEditor;
+		public MainWindow()
+		{
+			InitializeComponent();
+			_regestryEditor = new RegestryEditor();
+			foreach (var item in _regestryEditor.Items)
+			{
+				RegistryKeys.Items.Add(item);
+			}
+			
+		}
+	}
+}
