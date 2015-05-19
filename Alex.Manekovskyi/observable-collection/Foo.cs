d913a448f838dee4257be486ec2a5e7e8fb1efd2
manekovskiy <manekovskiy@gmail.com>
2015-05-19 21:35:11 +0300

    Showed how we can use DebuggerDisplay and DebuggerBrowsable attributes.

--- Alex.Manekovskyi/observable-collection/Foo.cs
+++ Alex.Manekovskyi/observable-collection/Foo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace observable_collection
 {
+	[DebuggerDisplay("{DebuggerDisplay,nq}")]
 	public class Foo : INotifyPropertyChanged
 	{
 		private int _id;
@@ -25,6 +27,7 @@ namespace observable_collection
 			}
 		}
 
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
 		public string Name
 		{
 			get { return _name; }
@@ -35,6 +38,11 @@ namespace observable_collection
 				_name = value;
 				InvokePropertyChanged();
 			}
+		}
+
+		private string DebuggerDisplay
+		{
+			get { return string.Format("Id = {0}; Name = {1}", Id, Name); }
 		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
