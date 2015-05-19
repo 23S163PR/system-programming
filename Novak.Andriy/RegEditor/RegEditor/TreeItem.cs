31cb44c3770ca2ec3d553464d3128975528d6ef9
Andriy Novak <kruzerandriy@gmail.com>
2015-05-19 04:06:55 +0300

    Change struct items model

--- Novak.Andriy/RegEditor/RegEditor/TreeItem.cs
+++ Novak.Andriy/RegEditor/RegEditor/TreeItem.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections.ObjectModel;
+using Microsoft.Win32;
 
 namespace RegEditor
 {
 	class TreeItem
 	{
-		public string Title { get; set; }
+		public string Title { get; set;}
+		public RegistryKey Key { get; set; }
 
 		public ObservableCollection<TreeItem> Items { get; set; }
 
-		public TreeItem()
+		public TreeItem(RegistryKey key, string title)
 		{
+			Key = key;
+			Title = title;
 			Items = new ObservableCollection<TreeItem>();
 		}	
 	}
