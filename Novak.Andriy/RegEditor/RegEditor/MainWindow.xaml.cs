ca523c12dc00944834d6159ed277a6204c61120b
Andriy Novak <kruzerandriy@gmail.com>
2015-05-19 04:09:07 +0300

    change xaml and main window class

--- Novak.Andriy/RegEditor/RegEditor/MainWindow.xaml.cs
+++ Novak.Andriy/RegEditor/RegEditor/MainWindow.xaml.cs
@@ -4,16 +4,30 @@ namespace RegEditor
 {
 	public partial class MainWindow : Window
 	{
-		private readonly RegestryEditor _regestryEditor;
+		private readonly RegistryEditor _registryEditor;
 		public MainWindow()
 		{
 			InitializeComponent();
-			_regestryEditor = new RegestryEditor();
-			foreach (var item in _regestryEditor.Items)
+			
+			_registryEditor = new RegistryEditor();
+			foreach (var item in _registryEditor.Items)
 			{
 				RegistryKeys.Items.Add(item);
 			}
-			
+		}
+
+		private void RegistryKeys_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+		{
+			var item = RegistryKeys.SelectedItem as TreeItem;
+			if (item == null) return;
+			item.Items.Clear();
+			_registryEditor.GetChildKeys(ref item);
+		}
+
+		private void RegistryKeys_OnExpanded(object sender, RoutedEventArgs e)
+		{
+			var item = sender as TreeItem;
+			_registryEditor.GetChildKeys(ref item);
 		}
 	}
 }
