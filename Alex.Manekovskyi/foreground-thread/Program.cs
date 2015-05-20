92777133f3a1c7e39f40a7e477cd06c4760df51c
manekovskiy <manekovskiy@gmail.com>
2015-05-20 13:09:49 +0300

    Added sample project showing how foreground thread affects process lifetime

---
+++ Alex.Manekovskyi/foreground-thread/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace foreground_thread
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// The process cannot be stopped until all foreground threads are not stopped
+			var thread = new Thread(() =>
+			{
+				Thread.Sleep(5000 /* 5 sec */);
+			});
+			thread.IsBackground = false;
+			thread.Start();
+		}
+	}
+}
