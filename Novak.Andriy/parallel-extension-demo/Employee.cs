c3c605ac7ab72697cddee1a925a81335c448419c
Andriy Novak <kruzerandriy@gmail.com>
2015-05-22 02:04:50 +0300

    Add serialize atributes for Employee

---
+++ Novak.Andriy/parallel-extension-demo/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace parallel_extension_demo
+{
+    [Serializable]
+    [XmlRoot("employee")]
+    public class Employee
+    {
+        [XmlElement(ElementName = "identity")]
+        public Guid Identity { get; set; }
+
+         [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+
+         [XmlElement(ElementName = "email")]
+        public string Email { get; set; }
+
+        [XmlElement(ElementName = "ageInYears")]
+        public int AgeInYears { get; set; }
+
+         [XmlElement(ElementName = "gender")]
+        public Gender Gender { get; set; }
+
+         [XmlElement(ElementName = "salary")]
+        public decimal Salary { get; set; }
+
+    }
+    
+    public enum Gender
+    {
+        Men,
+        Woman,
+        Transgender
+    }
+}
