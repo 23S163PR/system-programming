7c490b0923c5a447657cbb01d4ccb0526bf0511c
manekovskiy <manekovskiy@gmail.com>
2015-05-21 14:59:21 +0300

    Added sample project demonstrating difference between new and override keywords

---
+++ Alex.Manekovskyi/new-vs-override/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_vs_override
+{
+	class A
+	{
+		public virtual void Do()
+		{
+			Console.WriteLine("A");
+		}
+	}
+
+	class B : A
+	{
+		public override void Do()
+		{
+			Console.WriteLine("B");
+		}
+	}
+
+	class C : B
+	{
+		public new void Do()
+		{
+			Console.WriteLine("C");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			A a = new A();
+			A b = new B();
+			A c = new C();
+
+			a.Do();
+			b.Do();
+			c.Do();
+		}
+	}
+}
---
+++ Alex.Manekovskyi/new-vs-override/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("new-vs-override")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("new-vs-override")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e35a08d0-ce81-44ed-818a-95848ed4a8c0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
