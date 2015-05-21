1ca54882d8868797d1b15b5f4720ba185476758a
Andriy Novak <kruzerandriy@gmail.com>
2015-05-22 02:06:05 +0300

    Add Groping class for Employee

---
+++ Novak.Andriy/parallel-extension-demo/Groups.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace parallel_extension_demo
+{
+    [XmlRoot("groop")]
+    public class Groups
+    {
+        [XmlArray("employees"), XmlArrayItem("employee")]
+        public List<Employee> Colection { get; set; }
+        [XmlAttribute("employeeCount")]
+        public long Count { get; set; }
+
+        public Groups() { }
+
+        public Groups(List<Employee> list)
+        {
+            Colection = list; //.AddRange(list);
+            Count = Colection.Count;
+        }
+        
+    }
+}
