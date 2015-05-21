0904cb558fb6d5d4433af04fb6f5bce972a05ca1
manekovskiy <manekovskiy@gmail.com>
2015-05-21 18:12:42 +0300

    Added sample projects that generated 50000 objects in parallel

---
+++ Alex.Manekovskyi/parallel-extension-demo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parallel_extension_demo
+{
+	public class Employee
+	{
+		public Guid Identity { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public int AgeInYears { get; set; }
+		public Gender Gender { get; set; }
+		public decimal Salary { get; set; }
+	}
+
+	public enum Gender
+	{
+		Men,
+		Woman,
+		Transgender
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const int PeopleCount = 50000;
+			var people = new List<Employee>(PeopleCount);
+
+			var stopWatch = new Stopwatch();
+			stopWatch.Start();
+
+			Parallel.For(0, PeopleCount, i =>
+			{
+				var name = Faker.Name.FullName();
+				var employee = new Employee
+				{
+					Identity = Guid.NewGuid(),
+					Name = name,
+					Email = Faker.Internet.Email(name),
+					AgeInYears = Faker.RandomNumber.Next(18, 80),
+					Salary = Faker.RandomNumber.Next(10000, 30000), // $
+					Gender = Faker.Extensions.EnumExtensions.Rand<Gender>()
+				};
+
+				people.Add(employee);
+			});
+
+			stopWatch.Stop();
+			Console.WriteLine("Data generation completed in {0}ms", stopWatch.ElapsedMilliseconds);
+		}
+	}
+}
