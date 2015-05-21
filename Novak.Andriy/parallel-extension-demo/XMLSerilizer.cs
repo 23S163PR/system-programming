6a66d0649a8fd765d5b19247ad34dbbf1f5103a8
Andriy Novak <kruzerandriy@gmail.com>
2015-05-22 02:04:08 +0300

    Add static class for Serialize

---
+++ Novak.Andriy/parallel-extension-demo/XMLSerilizer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace parallel_extension_demo
+{
+    public static class XSerializer
+    {
+        public static void XSerilizer<T>(T obj, string fileName)
+        {
+            try
+            {
+                if (obj == null) return;
+                var xmlSerializer = new XmlSerializer(typeof(T));
+                using (var fs = new FileStream(string.Format(@"../../Groups Employee/{0}",fileName), FileMode.OpenOrCreate))
+                {
+                    xmlSerializer.Serialize(fs, obj);
+                    fs.Flush();
+                }
+            }
+            catch (SerializationException xe)
+            {
+                Console.WriteLine(xe.Message);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
