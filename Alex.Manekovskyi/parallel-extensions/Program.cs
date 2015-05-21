9a81cb008f8cdea85480b8dcd6fe81fe136948b9
manekovskiy <manekovskiy@gmail.com>
2015-05-21 18:46:26 +0300

    Added sample project showing Parallel Extensions

---
+++ Alex.Manekovskyi/parallel-extensions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace parallel_extensions
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Parallel.For(0, 200000, new ParallelOptions { MaxDegreeOfParallelism = 2 }, i =>
+			{
+				Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+			});
+
+			Parallel.ForEach(Enumerable.Range(0, 1000), i =>
+			{
+				Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+			});
+
+			Enumerable.Range(0, 1000)
+				.AsParallel()
+				.ForAll(i =>
+				{
+					Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+				});
+		}
+	}
+}
