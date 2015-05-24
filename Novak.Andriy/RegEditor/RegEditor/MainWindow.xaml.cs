d904e8843c5041d068697e0be276d832c97cc1d9
Andriy Novak <kruzerandriy@gmail.com>
2015-05-24 20:42:05 +0300

    add opportunity view keys in data gris

--- Novak.Andriy/RegEditor/RegEditor/MainWindow.xaml.cs
+++ Novak.Andriy/RegEditor/RegEditor/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using Microsoft.Win32;
 
 namespace RegEditor
 {
@@ -10,24 +12,38 @@ namespace RegEditor
 			InitializeComponent();
 			
 			_registryEditor = new RegistryEditor();
-			foreach (var item in _registryEditor.Items)
-			{
-				RegistryKeys.Items.Add(item);
-			}
-		}
+
+            foreach (var item in _registryEditor.Items)
+            {
+                RegistryKeys.Items.Add(item);
+            }
+		} 
 
 		private void RegistryKeys_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
 		{
-			var item = RegistryKeys.SelectedItem as TreeItem;
-			if (item == null) return;
-			item.Items.Clear();
-			_registryEditor.GetChildKeys(ref item);
+            var item = RegistryKeys.SelectedItem as TreeItem;
+            if (item == null) return;
+            item.ListItems.Clear();
+            var res = _registryEditor.GetChildKeys(item);
+            if (res == null) return;
+             item.ListItems.Clear();
+            foreach (var bar in res)
+		    {
+                item.ListItems.Add(bar);
+		    }
+            DataGridInfo(item.Key);
 		}
 
-		private void RegistryKeys_OnExpanded(object sender, RoutedEventArgs e)
-		{
-			var item = sender as TreeItem;
-			_registryEditor.GetChildKeys(ref item);
-		}
+	    private void DataGridInfo(RegistryKey key)
+	    {
+	        var res = key.GetValueNames().Select(p => new
+	        {
+	            Name = p//.Any() ? p : "default"
+                ,Type = key.GetValueKind(p)
+                ,Value=key.GetValue(p)
+	        });
+	        InfoDataGrid.DataContext = res;
+            
+	    }
 	}
 }
