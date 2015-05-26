bb514cbe1884451162ac058349439fb177632609
PavloZalevsky <pavlo.zalevsky@gmail.com>
2015-05-26 15:57:06 +0300

    sorting algorithms

---
+++ Zalevskyj.Pavlo/sort/sort/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            var arr = new int[1000000];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(0, 40);
+            }
+
+            Console.WriteLine();
+
+
+
+
+            //bubbleSort(arr);
+            Quicksort(arr, 0, arr.Length - 1);
+            //SelectionSort(arr);
+          //  mergesort(arr, 0, arr.Length - 1);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i].ToString() + " ");
+            }
+
+        }
+
+        static void bubbleSort(int[] arr)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int b = arr[i]; //change for elements
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = b;
+                    }
+                }
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("Data generation completed in {0}ms", stopWatch.ElapsedMilliseconds);
+        }
+
+        static void Quicksort(int[] arr, int left, int right)
+        {
+            int i = left, j = right;
+            IComparable pivot = arr[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (arr[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (arr[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                Quicksort(arr, left, j);
+            }
+
+            if (i < right)
+            {
+                Quicksort(arr, i, right);
+            }
+        }
+
+        static void SelectionSort(int[] arr)
+        {
+            int i, j, min, temp;
+            for (i = 0; i < arr.Length - 1; i++)
+            {
+                min = i;
+                for (j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+        static void mergeArray(int[] arr, int start, int mid, int end)
+        {
+            /* Create a temporary array for stroing merged array (Length of temp array will be 
+             * sum of size of both array to be merged)*/
+            int[] temp = new int[end - start + 1];
+
+            int i = start, j = mid + 1, k = 0;
+            // Now traverse both array simultaniously and store the smallest element of both to temp array
+            while (i <= mid && j <= end)
+            {
+                if (arr[i] < arr[j])
+                {
+                    temp[k] = arr[i];
+                    k++;
+                    i++;
+                }
+                else
+                {
+                    temp[k] = arr[j];
+                    k++;
+                    j++;
+                }
+            }
+            // If there is any element remain in first array then add it to temp array
+            while (i <= mid)
+            {
+                temp[k] = arr[i];
+                k++;
+                i++;
+            }
+            // If any element remain in second array then add it to temp array
+            while (j <= end)
+            {
+                temp[k] = arr[j];
+                k++;
+                j++;
+            }
+            // Now temp has merged sorted element of both array
+
+            // Traverse temp array and store element of temp array to original array
+            k = 0;
+            i = start;
+            while (k < temp.Length && i <= end)
+            {
+                arr[i] = temp[k];
+                i++;
+                k++;
+            }
+        }
+        // Recursive Merge Procedure
+        static void mergesort(int[] arr, int start, int end)
+        {
+            if (start < end)
+            {
+                int mid = (end + start) / 2;
+                mergesort(arr, start, mid);
+                mergesort(arr, mid + 1, end);
+                mergeArray(arr, start, mid, end);
+            }
+        }
+
+    }
+}
+
+
