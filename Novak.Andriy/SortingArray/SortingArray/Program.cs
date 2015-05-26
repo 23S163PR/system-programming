686849249a5cb39438629ecaa76656410bd504de
Andriy Novak <kruzerandriy@gmail.com>
2015-05-26 03:59:18 +0300

    Create New Sorting Array

---
+++ Novak.Andriy/SortingArray/SortingArray/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SortingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var bubleArr = RandArr(5000, new Random());
+            var quickArr = (int[])bubleArr.Clone();
+            var selectionArr = (int[])bubleArr.Clone();
+            var mergArr = (int[])bubleArr.Clone();
+            var task = new TaskFactory(
+                TaskCreationOptions.AttachedToParent,
+                TaskContinuationOptions.None);
+            //inclusive 40%
+            var tasks = new[]
+            {
+                /*inclusive samples 20%*/
+                 task.StartNew(() => GetTime(() => SortArray.BubleSort(bubleArr)))
+                 /*inclusive samples 10%*/  // 
+                 ,task.StartNew(() => GetTime(() => SortArray.QuickSort(quickArr,0,quickArr.Length-1)))
+                //samples 14.29
+                ,task.StartNew(() => GetTime(() => SortArray.SelectionSort(selectionArr)))
+                /*inclusive samples 30%*/
+                ,task.StartNew(() => GetTime(() => SortArray.MergeSort(mergArr,0,mergArr.Length-1)))   
+            };
+
+            foreach (var t in tasks)
+            {
+                Console.WriteLine("\n{0} Miliseconds", t.Result);
+            }
+        }
+
+        static double GetTime(Func<int[]> func)
+        {
+            var stopWotch = new Stopwatch();
+            stopWotch.Start();
+            func();
+            stopWotch.Stop();
+            return stopWotch.Elapsed.TotalMilliseconds;
+        }
+
+        static int[] RandArr(int count, Random rnd)
+        {
+            var arr = new int[count];
+
+            for (var i = 0; i++ < count - 1; )
+            {
+                arr[i] = rnd.Next(0, 500);
+            }
+            return arr;
+        }
+    }
+}
---
+++ Novak.Andriy/SortingArray/SortingArray/SortArray.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace SortingArray
+{
+    static class SortArray
+    {
+        public static int[] BubleSort(int[] array)
+        {
+            for (int write = 0; write < array.Length; write++)
+            {
+                for (int sort = 0; sort < array.Length - 1; sort++)
+                {
+                    if (array[sort] > array[sort + 1]) //load
+                    {
+                        var temp = array[sort + 1];
+                        array[sort + 1] = array[sort];
+                        array[sort] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+        public static int[] QuickSort(int[] elements, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = elements[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (elements[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (elements[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    var tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+            // Recursive calls
+            if (left < j)
+            {
+                QuickSort(elements, left, j);
+            }
+
+            if (i < right)
+            {
+                QuickSort(elements, i, right);
+            }
+
+            return elements;
+        }
+
+        public static int[] SelectionSort(int[] array)
+        {
+            int i, j, min, temp;
+            for (i = 0; i < array.Length - 1; i++)
+            {
+                min = i;
+                for (j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])  // load
+                    {
+                        min = j;
+                    }
+                }
+                temp = array[i];
+                array[i] = array[min];
+                array[min] = temp;
+            }
+            return array;
+        }
+
+        public static int[] MergeSort(int[] input, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+
+                MergeSort(input, left, middle);                 //maximum load
+                MergeSort(input, middle + 1, right);            //maximum load
+                //maximum load
+                //Merge                                         //maximum load
+                int[] leftArray = new int[middle - left + 1];   //maximum load
+                int[] rightArray = new int[right - middle];     //maximum load
+
+                Array.Copy(input, left, leftArray, 0, middle - left + 1);
+                Array.Copy(input, middle + 1, rightArray, 0, right - middle);
+
+                int i = 0;
+                int j = 0;
+                for (int k = left; k < right + 1; k++)  //load
+                {
+                    if (i == leftArray.Length)
+                    {
+                        input[k] = rightArray[j];
+                        j++;
+                    }
+                    else if (j == rightArray.Length)
+                    {
+                        input[k] = leftArray[i];
+                        i++;
+                    }
+                    else if (leftArray[i] <= rightArray[j])
+                    {
+                        input[k] = leftArray[i];
+                        i++;
+                    }
+                    else
+                    {
+                        input[k] = rightArray[j];
+                        j++;
+                    }
+                }
+            }
+            return input;
+        }
+
+
+    }
+}
