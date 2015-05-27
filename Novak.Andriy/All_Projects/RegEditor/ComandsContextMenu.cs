910a9023b5ccc0c444ab94504b6473dac46692c3
Andriy Novak <kruzerandriy@gmail.com>
2015-05-28 02:18:04 +0300

    add comands for context menu RegEditir

---
+++ Novak.Andriy/All_Projects/RegEditor/ComandsContextMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace RegEditor
+{
+    public static class ComandsContextMenu 
+    {
+        public static readonly RoutedUICommand CreateKey = new RoutedUICommand(
+            "Create Key", "CreateKey", typeof(MainWindow));
+
+        public static readonly RoutedUICommand UpdateKey = new RoutedUICommand(
+            "Update Key", "UpdateKey", typeof(MainWindow));
+
+        public static readonly RoutedUICommand DeleteKey = new RoutedUICommand(
+            "Delete Key", "DeleteKey", typeof(MainWindow));
+    }
+}
