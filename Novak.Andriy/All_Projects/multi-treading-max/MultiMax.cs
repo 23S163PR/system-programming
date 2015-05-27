0b72573fb7c3e09b7e6be171f8ac2f31ec6822fe
Andriy Novak <kruzerandriy@gmail.com>
2015-05-27 22:53:33 +0300

    Init Multi Max progect
    Profiler show results volatile and interloced, interloced win in results

---
+++ Novak.Andriy/All_Projects/multi-treading-max/MultiMax.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+
+namespace multi_treading_max
+{
+    static class MultiMax
+    {
+        public static void Max(ref int max, int value)
+        {
+            if (max < value)
+            {
+                Interlocked.Exchange(ref max, value);
+            }
+        }
+    }
+}
---
+++ Novak.Andriy/All_Projects/multi-treading-max/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace multi_treading_max
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            var arr = new List<int>();
+            var rnd = new Random();
+            for (var i = 0; i < 500000; i++)
+            {
+                arr.Add(rnd.Next(100) +i*2);
+            }
+
+            int max = 0;
+            Parallel.ForEach(arr, i =>
+            {
+                MultiMax.Max(ref max, i);
+            });
+
+            Console.WriteLine("\nmax in arr - {0}", arr.Max());
+            Console.WriteLine("max in multimax - {0}", max);
+        }
+    }
+}
