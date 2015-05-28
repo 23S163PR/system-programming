ad423e9e1e4794181c552785141ae48fc76d90f9
manekovskiy <manekovskiy@gmail.com>
2015-05-28 13:15:58 +0300

    Added sample project showing how to implement recursive locks using AutoResetEvent

---
+++ Alex.Manekovskyi/recursive-autoresetevent/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace recursive_autoresetevent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    internal class RecursiveAutoResetEvent: IDisposable
+    {
+        private AutoResetEvent autoResetEvent = new AutoResetEvent(initialState: true);
+        private int ownerThreadId = 0;
+        private int counter = 0;
+
+        public void Enter()
+        {
+            int currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            if (ownerThreadId == currentThreadId)
+            {
+                counter++;
+                return;
+            }
+
+            autoResetEvent.WaitOne();
+
+            ownerThreadId = currentThreadId;
+            counter = 1;
+        }
+
+        public void Leave()
+        {
+            int currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            if (ownerThreadId != currentThreadId)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (--counter == 0)
+            {
+                ownerThreadId = 0;
+                autoResetEvent.Set();
+            }
+        }
+
+        public void Dispose()
+        {
+            autoResetEvent.Dispose();
+        }
+    }
+}
