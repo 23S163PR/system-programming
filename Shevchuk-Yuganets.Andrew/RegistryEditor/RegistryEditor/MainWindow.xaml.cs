9ea9add177a0eefbee8fcc16f1cd4a155d99385a
Andrew Shevchuk-Yuganets <mazillko@gmail.com>
2015-06-04 16:20:19 +0300

    Added default registry branches and expand, collapse events

--- Shevchuk-Yuganets.Andrew/RegistryEditor/RegistryEditor/MainWindow.xaml.cs
+++ Shevchuk-Yuganets.Andrew/RegistryEditor/RegistryEditor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace RegistryEditor
 {
@@ -23,6 +24,74 @@ namespace RegistryEditor
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+		{
+			// creating registry branches
+			var classesRoot = new TreeViewItem
+			{
+				Tag = Registry.ClassesRoot,
+				Header = Registry.ClassesRoot.Name,
+				Items = { "*" }
+			};
+
+			var currentUser = new TreeViewItem
+			{
+				Tag = Registry.CurrentUser,
+				Header = Registry.CurrentUser.Name,
+				Items = { "*" }
+			};
+
+			var localMachine = new TreeViewItem
+			{
+				Tag = Registry.LocalMachine,
+				Header = Registry.LocalMachine.Name,
+				Items = { "*" }
+			};
+
+			var users = new TreeViewItem
+			{
+				Tag = Registry.Users,
+				Header = Registry.Users.Name,
+				Items = { "*" }
+			};
+
+			var currentConfig = new TreeViewItem
+			{
+				Tag = Registry.CurrentConfig,
+				Header = Registry.CurrentConfig.Name,
+				Items = { "*" }
+			};
+
+			var computer = new TreeViewItem
+			{
+				Tag = null,
+				Header = "Computer",
+				Items = { classesRoot, currentUser, localMachine, users, currentConfig } // add registry branches to default tree view item "computer"
+			};
+			RegistryTreeView.Items.Add(computer);
+		}
+
+
+		private void TreeViewItem_OnExpanded(object sender, RoutedEventArgs e)
+		{
+			var treeViewItem = e.OriginalSource as TreeViewItem;
+
+			if (treeViewItem == null) return;
+			if (treeViewItem.Tag == null) // check if expanded item not a "computer"
+			{
+				return;
+			}
+			else
+			{
+				// add new items from branch
+			}
+		}
+
+		private void TreeViewItem_OnCollapsed(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
