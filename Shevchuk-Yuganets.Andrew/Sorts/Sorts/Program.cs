ede3c06524711fc9634955c93b0d0141ef9a38be
Andrew Shevchuk-Yuganets <mazillko@gmail.com>
2015-06-12 16:05:55 +0300

    Code refactoring

--- Shevchuk-Yuganets.Andrew/Sorts/Sorts/Program.cs
+++ Shevchuk-Yuganets.Andrew/Sorts/Sorts/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Sorts
 {
-	struct Result
+	public struct Result
 	{
 		public string Name;
 		public double Time;
@@ -15,15 +16,18 @@ namespace Sorts
 	{
 		private static void Main(string[] args)
 		{
+			Sort();
+			Console.ReadLine();
+		}
+
+		async static void Sort()
+		{
 			var bubble = GetRandomArray(10000); // 10000 - size of int array
 			var quick = (int[])bubble.Clone();
 			var selection = (int[])bubble.Clone();
 			var merge = (int[])bubble.Clone();
 
-			var taskFactory = new TaskFactory(
-				TaskCreationOptions.AttachedToParent,
-				TaskContinuationOptions.None);
-
+			var taskFactory = new TaskFactory();
 			var tasks = new[]
 			{
 				taskFactory.StartNew(() => GetRuntime(() => Sorts.BubleSort(bubble))),
@@ -31,11 +35,26 @@ namespace Sorts
 				taskFactory.StartNew(() => GetRuntime(() => Sorts.SelectionSort(selection))),
 				taskFactory.StartNew(() => GetRuntime(() => Sorts.MergeSort(merge,0,merge.Length-1)))
 			};
+			await Task.WhenAny(tasks)
+				.ContinueWith(t =>
+				{
+					Console.WriteLine("The champion is: {0} with {1}ms time!", 
+						t.Result.Result.Name, t.Result.Result.Time);
+				});
 
-            foreach (var res in tasks.OrderBy(res => res.Result.Time))
-			{
-				Console.WriteLine("{0, -20} : {1, -10} Miliseconds", res.Result.Name, res.Result.Time);
-			}
+			await Task.WhenAll(tasks)
+				.ContinueWith(t =>
+				{
+					foreach (var time in t.Result)
+					{
+						Graphics.TotalTime += time.Time;
+					}
+
+					foreach (var time in t.Result)
+					{
+						Graphics.Print(time);
+					}
+				});
 		}
 
 		static Result GetRuntime(Func<string> sortMethod)
@@ -61,6 +80,64 @@ namespace Sorts
 				array[i] = random.Next(0, 1000);
 			}
 			return array;
+		}
+	}
+
+	//
+	public static class Graphics
+	{
+		private static readonly Random Rand = new Random();
+
+		public static double TotalTime { get; set; }
+
+		private static int _xStartPos = 10;
+		private static int _yStartPos = 50;
+		private static int _columnWidth = 3;
+		private static int _distanceBetweenColumns = 20;
+
+		private static readonly List<int> ColorList = new List<int>();
+
+		public static void Print(Result result)
+		{
+			Console.ForegroundColor = GetRandomColor();
+
+			double value = (result.Time / TotalTime) * 100;
+
+			for (int y = 0; y < ((int)value / 2) + 1; y++)
+			{
+				for (int i = 0; i < _columnWidth; i++)
+				{
+					Console.SetCursorPosition(_xStartPos + i, _yStartPos - y);
+					Console.Write(Encoding.GetEncoding(437 /* █ */).GetChars(new byte[] { 219 })[0]);
+				}
+			}
+
+			// sort percentage line
+			Console.SetCursorPosition(_xStartPos - 1, _yStartPos + 2);
+			Console.WriteLine("{0}%", value.ToString("0.00"));
+
+			// sort name line
+			Console.SetCursorPosition(_xStartPos - (result.Name.Length / 2) + 1, _yStartPos + 3);
+			Console.WriteLine(result.Name);
+
+			_xStartPos += _distanceBetweenColumns;
+		}
+
+		private static ConsoleColor GetRandomColor()
+		{
+			ConsoleColor color;
+			int numberColor;
+
+			do
+			{
+				numberColor = Rand.Next(9, 15);
+			} while (ColorList.Contains(numberColor));
+
+			ColorList.Add(numberColor);
+
+			Enum.TryParse(numberColor.ToString(), out color);
+
+			return color;
 		}
 	}
 }
