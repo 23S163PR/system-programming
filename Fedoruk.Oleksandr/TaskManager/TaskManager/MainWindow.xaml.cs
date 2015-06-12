d950ad50797095e5df47e441191bc700e86eb736
Sasha Fedoruk <sasha1.fedoruk@gmail.com>
2015-06-12 14:32:54 +0300

    Update Code

--- Fedoruk.Oleksandr/TaskManager/TaskManager/MainWindow.xaml.cs
+++ Fedoruk.Oleksandr/TaskManager/TaskManager/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using TaskManager.Classes;
 using System.Linq;
 using System;
 using System.Collections.ObjectModel;
+using System.IO;
 
 namespace TaskManager
 {
@@ -33,7 +34,7 @@ namespace TaskManager
 
             System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += (s, e) => UpdateProcessesInfo();
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
             dispatcherTimer.Start();
         }
 
@@ -42,19 +43,15 @@ namespace TaskManager
             var collection = new ObservableCollection<ProcessInfo>();
             foreach (var el in Process.GetProcesses())
             {
-                try
-                {
-                    var workingSetInB = new PerformanceCounter("Process", "Working Set - Private", el.ProcessName).RawValue;
-                    var cpuUssingCounter = new PerformanceCounter("Process", "% Processor Time", el.ProcessName);
-                    collection.Add(new ProcessInfo(el.Id
-                                             , el.ProcessName
-                                             , el.Threads.Count
-                                             , workingSetInB
-                                             , cpuUssingCounter));
-                }
-                catch (Exception)
-                {
-                }
+                var workingSetCounter = GetPerformanceCounter(el, "Working Set - Private");
+                if (workingSetCounter == null) continue;
+                var cpuUssingCounter = GetPerformanceCounter(el, "% Processor Time");
+                collection.Add(new ProcessInfo(el.Id
+                                         , el.ProcessName
+                                         , el.Threads.Count
+                                         , workingSetCounter.RawValue
+                                         , cpuUssingCounter));
+
             }
             return collection;
         }
@@ -64,29 +61,25 @@ namespace TaskManager
             foreach (var el in Process.GetProcesses())
             {
                 ProcessInfo proc = Processes.FirstOrDefault(x => x.ProcessId == el.Id);
-                try
+                var workingSetCounter = GetPerformanceCounter(el, "Working Set - Private");
+                if (workingSetCounter == null) continue;
+                if (proc == null)
                 {
-                    var workingSetInB = new PerformanceCounter("Process", "Working Set - Private", el.ProcessName).RawValue;
-                    if (proc == null)
-                    {
-                        var cpuUssingCounter = new PerformanceCounter("Process", "%Processor Time", el.ProcessName);
-                        Processes.Add(new ProcessInfo(el.Id
-                                                      , el.ProcessName
-                                                      , el.Threads.Count
-                                                      , workingSetInB
-                                                      , cpuUssingCounter));
-                    }
-                    else
-                    {
-                        proc.UpdateInfo(el.Threads.Count
-                                        , workingSetInB);
-                    }
+                    var cpuUssingCounter = GetPerformanceCounter(el, "% Processor Time");
+                    Processes.Add(new ProcessInfo(el.Id
+                                                  , el.ProcessName
+                                                  , el.Threads.Count
+                                                  , workingSetCounter.RawValue
+                                                  , cpuUssingCounter));
                 }
-                catch (Exception)
+                else
                 {
+                    proc.UpdateInfo(el.Threads.Count
+                                    , workingSetCounter.RawValue);
                 }
+
             }
-            for (int i = Processes.Count - 1; i >= 0; i--)
+            for (int i = 0; i < Processes.Count; i++)
             {
                 var proc = Process.GetProcesses().FirstOrDefault(x => x.Id == Processes[i].ProcessId);
                 if (proc == null)
@@ -94,6 +87,27 @@ namespace TaskManager
                     Processes.Remove(Processes[i]);
                 }
             }
+        }
+
+        public PerformanceCounter GetPerformanceCounter(Process process, string processCounterName)
+        {
+            var processName = Path.GetFileNameWithoutExtension(process.ProcessName);
+            var processCategory = new PerformanceCounterCategory("Process");
+            var similarInstances = processCategory.GetInstanceNames()
+                .Where(instance => instance.StartsWith(processName));
+
+            foreach (var instance in similarInstances)
+            {
+                using (var processIdCounter = new PerformanceCounter("Process", "ID Process", instance, true /* readOnly */))
+                {
+                    int instanceId = (int)processIdCounter.RawValue;
+                    if (instanceId == process.Id)
+                    {
+                        return new PerformanceCounter("Process", processCounterName, instance);
+                    }
+                }
+            }
+            return null;
         }
     }
 }
